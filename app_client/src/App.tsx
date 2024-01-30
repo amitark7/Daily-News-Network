8d3e53b61a54bcfce06a3b28ff5d5e8d98ef6c1e
amitark7 <amitkumar1600121@gmail.com>
2024-01-31 02:59:47 +0530

    Update App.tsx

--- app_client/src/App.tsx
+++ app_client/src/App.tsx
@@ -1,8 +1,5 @@
-import {StatusBar, StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet} from 'react-native';
 import React from 'react';
-import Header from './component/Header';
-import News from './component/News';
-import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import Splash from './screens/Splash';
@@ -14,27 +11,6 @@ export type StackProp = {
   Splash: undefined;
 };
 const Stack = createNativeStackNavigator<StackProp>();
-const Tab = createMaterialTopTabNavigator();
-// export const HomeTabs = () => {
-//   return (
-//     <>
-//       <Header />
-//       <Tab.Navigator
-//         screenOptions={{
-//           tabBarAndroidRipple: {borderless: false},
-          
-//         }}>
-//         <Tab.Screen name="Home" component={Home} />
-//         <Tab.Screen name="Sports" component={Sports} />
-//         <Tab.Screen name="Science" component={Science} />
-//         {/* <Tab.Screen name="Technology" component={Technology} /> */}
-//         <Tab.Screen name="Health" component={Health} />
-//         {/* <Tab.Screen name="Entertainment" component={Entertainment} />
-//         <Tab.Screen name="Business" component={Business} /> */}
-//       </Tab.Navigator>
-//     </>
-//   );
-// };
 const App = () => {
   return (
     <NewsProvider>
